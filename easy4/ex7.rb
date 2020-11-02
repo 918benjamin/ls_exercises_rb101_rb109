9f8971c9524d84a9d65e6212447e1e552912a1b4
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-11-02 09:23:59 -0500

    Add further exploration to Easy4 exercise 7

--- easy4/ex7.rb
+++ easy4/ex7.rb
@@ -61,5 +61,31 @@ def string_to_integer(str)
 end
 
 # Test cases
-p string_to_integer('4321') == 4321
-p string_to_integer('570') == 570
+# p string_to_integer('4321') == 4321
+# p string_to_integer('570') == 570
+
+# Further exploration
+# Write a hexadecimal_to_integer method that converts a string representing a
+# hexadecimal number to its integer value.
+
+# Used the given solution as a base for this further exploration, to get some
+# familiarity with this way of solving the problem.
+
+HEX_DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
+  '7' => 7, '8' => 8, '9' => 9, 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13,
+  'E' => 14, 'F' => 15
+}
+
+def hexadecimal_to_integer(string)
+  digits = string.chars.map { |char| HEX_DIGITS[char.upcase] }
+
+  value = 0
+  digits.each { |digit| value = 16 * value + digit }
+  value
+end
+
+p hexadecimal_to_integer('4D9f') == 19871
+
+
+
