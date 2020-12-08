39b9ca039f25a7bb6cfec8c9b1b238182ab1d666
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-08 11:02:58 -0500

    Add completed ex2 of easy7

---
+++ easy7/ex2.rb
@@ -0,0 +1,40 @@
+=begin
+Write a method that takes a string, and then returns a hash that contains 3
+entries: one represents the number of characters in the string that are
+lowercase letters, one the number of characters that are uppercase letters,
+and one the number of characters that are neither.
+
+Examples:
+letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+
+Problem:
+- Input: string
+- Output: hash
+  - three key:value pairs
+    - lowercase: number of lowercased chars
+    - uppercase: number of uppercased chars
+    - neither: number of character that are neither
+
+Algorithm:
+- 
+
+
+=end
+
+def letter_case_count(str)
+  result = {}
+  result[:lowercase] = str.count('a-z')
+  result[:uppercase] = str.count('A-Z')
+  result[:neither] = str.count('^a-z', '^A-Z')
+  result
+end
+
+
+# Test cases
+p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
