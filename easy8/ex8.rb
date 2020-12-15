8a23e00a838e7b04e1749052f02a2bc97214964c
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-15 17:17:44 -0600

    Add completed ex8 of easy8

--- easy8/ex8.rb
+++ easy8/ex8.rb
@@ -11,10 +11,18 @@ double_consonants('') == ""
 
 
 =end
+def double_consonants(str)
+  doubled = ""
+  str.each_char do |char|
+    doubled += char if char.match(/[b-df-hj-np-tv-z]/i)
+    doubled += char
+  end
+  doubled
+end
 
 
 # Test cases
-double_consonants('String') == "SSttrrinngg"
-double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
-double_consonants("July 4th") == "JJullyy 4tthh"
-double_consonants('') == ""
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
