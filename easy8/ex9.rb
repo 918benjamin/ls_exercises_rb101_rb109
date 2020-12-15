b3d840d007ad8c581e4ca0f60943a5bfa36e514b
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-15 17:23:39 -0600

    Add completed ex9 of easy8

--- easy8/ex9.rb
+++ easy8/ex9.rb
@@ -14,14 +14,20 @@ reversed_number(12000) == 21 # No leading zeros in return value!
 reversed_number(12003) == 30021
 reversed_number(1) == 1
 
+Problem:
+- input: integer (positive)
+- 
 
 =end
 
+def reversed_number(int)
+  int.digits.join.to_i
+end
 
 # Test cases
-reversed_number(12345) == 54321
-reversed_number(12213) == 31221
-reversed_number(456) == 654
-reversed_number(12000) == 21 # No leading zeros in return value!
-reversed_number(12003) == 30021
-reversed_number(1) == 1
+p reversed_number(12345) == 54321
+p reversed_number(12213) == 31221
+p reversed_number(456) == 654
+p reversed_number(12000) == 21 # No leading zeros in return value!
+p reversed_number(12003) == 30021
+p reversed_number(1) == 1
