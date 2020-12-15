0e56d5e3bab6e4dbcf2c7a17f4d39fc02af07630
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-15 08:38:48 -0600

    Add completed ex6 of easy8

--- easy8/ex6.rb
+++ easy8/ex6.rb
@@ -8,9 +8,32 @@ print "FizzBuzz".
 Example:
 fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
 
+Problem:
+- inputs: two integers (starting num, ending num)
+- output: print all numbers between the two inputs with some swapped for strs
+  - if number divisible by 3 and 5, print FizzBuzz
+  - if number divisible by 3, print Fizz
+  - if number divisible by 5, print Buzz
+
 
 =end
 
+def fizzbuzz(start, stop)
+  str = []
+  1.upto(stop) do |num|
+    if num % 3 == 0 && num % 5 == 0
+      str << "FizzBuzz, "
+    elsif num % 3 == 0
+      str << "Fizz, "
+    elsif num % 5 == 0
+      str << "Buzz, "
+    else
+      str << "#{num.to_s}, "
+    end
+  end
+  str[-1].slice!(-2, 2)
+  puts str.join
+end
 
 # Test cases
 fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
