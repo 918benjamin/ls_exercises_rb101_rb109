b8d5bca29cf9a6e0dde52f062a1aed4d95d5761e
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-15 07:55:07 -0600

    Add completed ex3 of easy8

--- easy8/ex3.rb
+++ easy8/ex3.rb
@@ -8,11 +8,26 @@ leading_substrings('abc') == ['a', 'ab', 'abc']
 leading_substrings('a') == ['a']
 leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
 
+Problem:
+- input: string
+- output: array (of substrings)
+- substrings that start with the first letter
+- substring order is smallest to greatest
+
+Approaches:
+- Slice
 
 =end
 
+def leading_substrings(str)
+  sub_strs = []
+  1.upto(str.length) do |x|
+    sub_strs << str[0, x]
+  end
+  sub_strs
+end
 
 # Test cases
-leading_substrings('abc') == ['a', 'ab', 'abc']
-leading_substrings('a') == ['a']
-leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+p leading_substrings('abc') == ['a', 'ab', 'abc']
+p leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
