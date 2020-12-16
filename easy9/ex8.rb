e55079b87dd8f87459b418f2ab24211a74931712
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 09:47:47 -0600

    Add completed ex8 of easy9

--- easy9/ex8.rb
+++ easy9/ex8.rb
@@ -15,11 +15,41 @@ sequence(4, -7) == [-7, -14, -21, -28]
 sequence(3, 0) == [0, 0, 0]
 sequence(0, 1000000) == []
 
+Problem
+- inputs: integer (count), integer (start value)
+- output: array of integers
+  - same number of integers as first arg count
+  - each element is a multiple of the starting number
+
 =end
 
+### My solution
+def sequence(max, value)
+  (1..max).each_with_object([]) do |num, arr|
+    arr << value * num
+  end
+end
+
+### LS solution 1
+# def sequence(count, first)
+#   sequence = []
+#   number = first
+
+#   count.times do
+#     sequence << number
+#     number += first
+#   end
+
+#   sequence
+# end
+
+### LS solution 2
+# def sequence(count, first)
+#   (1..count).map { |value| value * first }
+# end
 
 # Test cases
-sequence(5, 1) == [1, 2, 3, 4, 5]
-sequence(4, -7) == [-7, -14, -21, -28]
-sequence(3, 0) == [0, 0, 0]
-sequence(0, 1000000) == []
+p sequence(5, 1) == [1, 2, 3, 4, 5]
+p sequence(4, -7) == [-7, -14, -21, -28]
+p sequence(3, 0) == [0, 0, 0]
+p sequence(0, 1000000) == []
