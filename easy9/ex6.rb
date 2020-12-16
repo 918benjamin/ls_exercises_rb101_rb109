9eace97545f2c6c262b9bdaa4225b7070b57d462
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 09:28:45 -0600

    Add completed ex6 of easy9

--- easy9/ex6.rb
+++ easy9/ex6.rb
@@ -8,30 +8,39 @@ and that any substring of non-space characters is a word.
 
 Examples
 word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
-
 word_lengths("baseball hot dogs and apple pie") ==
   ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
-
 word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
-
 word_lengths("Supercalifragilisticexpialidocious") ==
   ["Supercalifragilisticexpialidocious 34"]
-
 word_lengths("") == []
 
+Problem:
+- input: string
+- output: return Array
+  - each element is a word from the string + space + word length
+- one space separates each word
+
+Algorithm
+- Split the string into an array of words
+- iterate through the array transforming each word (map)
+  - add a space and the length of the word
 
 =end
 
+def word_lengths(str)
+  str.split.map { |word| word + " " + word.length.to_s}
+end
 
 # Test cases
-word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
 
-word_lengths("baseball hot dogs and apple pie") ==
+p word_lengths("baseball hot dogs and apple pie") ==
   ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
 
-word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
 
-word_lengths("Supercalifragilisticexpialidocious") ==
+p word_lengths("Supercalifragilisticexpialidocious") ==
   ["Supercalifragilisticexpialidocious 34"]
 
-word_lengths("") == []
+p word_lengths("") == []
