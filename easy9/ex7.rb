dc1c25efdfd3db931edcb65f2e285b24c9ed7a5f
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 09:31:33 -0600

    Add completed ex7 of easy9

--- easy9/ex7.rb
+++ easy9/ex7.rb
@@ -8,6 +8,16 @@ swap_name('Joe Roberts') == 'Roberts, Joe'
 
 =end
 
+### My solution
+# def swap_name(name)
+#   arr = name.split
+#   "#{arr[1]}, #{arr[0]}"
+# end
+
+### LS solution
+def swap_name(name)
+  name.split(' ').reverse.join(', ')
+end
 
 # Test cases
-swap_name('Joe Roberts') == 'Roberts, Joe'
+p swap_name('Joe Roberts') == 'Roberts, Joe'
