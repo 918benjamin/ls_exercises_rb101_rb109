0151e887fc48c9c5ca6637d0b2f8bb7ec6bc5d0d
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 08:51:25 -0600

    add completed ex3 of easy9

--- easy9/ex3.rb
+++ easy9/ex3.rb
@@ -8,11 +8,38 @@ negative(5) == -5
 negative(-3) == -3
 negative(0) == 0      # There's no such thing as -0 in ruby
 
+Problem:
+- input: integer
+- output: integer
+  - if input is >= 1 return negative input
+  - else, return input
+
+
 
 =end
 
+### My solution
+# def negative(int)
+#   return int if int <= 0
+#   int - int * 2
+# end
+
+### LS solution w/ Ternary
+# def negative(int)
+#   int > 0 ? -int : int
+# end
+
+### LS Solution from Further exploration
+def negative(number)
+  -number.abs
+end
+
+# This solution is shorter but may not be "superior" because it doesn't make
+# clear the intention to keep negative numbers negative and only change
+# positive numbers. It does accomplish this technically, but it doesn't maintain
+# the intent explicitely which could cause issues further on.
 
 # Test cases
-negative(5) == -5
-negative(-3) == -3
-negative(0) == 0      # There's no such thing as -0 in ruby
+p negative(5) == -5
+p negative(-3) == -3
+p negative(0) == 0      # There's no such thing as -0 in ruby
