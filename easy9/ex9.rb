2d46fa07fe817a40faecd6c6f04c51a291566f58
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 09:58:26 -0600

    Add completed ex9 of easy 9

--- easy9/ex9.rb
+++ easy9/ex9.rb
@@ -18,7 +18,31 @@ get_grade(50, 50, 95) == "D"
 
 =end
 
+### My solution
+# def get_grade(score1, score2, score3)
+#   avg = (score1 + score2 + score3) / 3
+#   case
+#   when avg >= 90 then "A"
+#   when avg >= 80 then "B"
+#   when avg >= 70 then "C"
+#   when avg >= 60 then "D"
+#   else "F"
+#   end
+# end
+
+### LS solution
+def get_grade(s1, s2, s3)
+  result = (s1 + s2 + s3)/3
+
+  case result
+  when 90..100 then 'A'
+  when 80..89  then 'B'
+  when 70..79  then 'C'
+  when 60..69  then 'D'
+  else              'F'
+  end
+end
 
 # Test cases
-get_grade(95, 90, 93) == "A"
-get_grade(50, 50, 95) == "D"
+p get_grade(95, 90, 93) == "A"
+p get_grade(50, 50, 95) == "D"
