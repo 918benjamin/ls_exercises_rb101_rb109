a251ac906bde623eb8a69560abe7bb754b1ea2ab
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 09:01:05 -0600

    Add completed ex4 of easy9

--- easy9/ex4.rb
+++ easy9/ex4.rb
@@ -11,8 +11,20 @@ sequence(1) == [1]
 
 =end
 
+### My original solution
+# def sequence(int)
+#   (1..int).to_a
+# end
+
+### Further exploration
+def sequence(int)
+  int >= 0 ? (1..int).to_a : (int..-1).to_a
+end
 
 # Test cases
-sequence(5) == [1, 2, 3, 4, 5]
-sequence(3) == [1, 2, 3]
-sequence(1) == [1]
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
+p sequence(-5) == [-5, -4, -3, -2, -1]
+p sequence(-3) == [-3, -2, -1]
+p sequence(-1) == [-1]
