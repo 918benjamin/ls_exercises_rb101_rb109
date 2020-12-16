f3a3e011e148acca0c9c52863f29a82495f3b830
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-16 10:09:16 -0600

    Add completed ex10 of easy9

--- easy9/ex10.rb
+++ easy9/ex10.rb
@@ -6,9 +6,31 @@ Example:
 buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
   ["apples", "apples", "apples", "orange", "bananas","bananas"]
 
+PROBLEM
+- input: array of subarrays [string fruit, integer quantity]
+- output: array
+  - contains as many fruit strings as there are quantity for each sub array, all as one flat array
+- 
+
+
 =end
 
+### My solution
+# def buy_fruit(fruit_arr)
+#   result = []
+#   fruit_arr.each do |arr|
+#     arr[1].times do |_|
+#       result << arr[0]
+#     end
+#   end
+#   result
+# end
+
+### LS soltion 2
+def buy_fruit(list)
+  list.map { |fruit, quantity| [fruit] * quantity }.flatten
+end
 
 # Test cases
-buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) ==
+p buy_fruit([["apples", 3], ["orange", 1], ["bananas", 2]]) #==
   ["apples", "apples", "apples", "orange", "bananas","bananas"]
