f706968007c9d78058d2cf717ecc599d386ace25
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-24 13:57:32 -0600

    Add completed problem that james gave me

---
+++ extra/james2.rb
@@ -0,0 +1,26 @@
+require "pry"
+require "pry-byebug"
+
+def weird_case(str)
+  binding.pry
+  chars = str.chars
+  flip = true
+  chars = chars.map do |char|
+    if char == ' '
+      char
+    elsif flip
+      flip = false
+      char.upcase!
+    else
+      flip = true
+      char
+    end
+  end
+  chars.join
+end
+
+
+# test cases
+p weird_case("String") #== "StRiNg"
+# p weird_case("Weird string case") #== "WeIrD sTrInG cAsE"
+# p weird_case("string") #== "StRiNg"
