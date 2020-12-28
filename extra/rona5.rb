ab6b453939da5a62a72fd2f9ea178c5779088031
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-28 10:03:04 -0600

    Add ronas solution to this problem for later review

--- extra/rona5.rb
+++ extra/rona5.rb
@@ -13,3 +13,27 @@ build_palindrome("ababab") == "abababa"
 build_palindrome("accccc") == "accccca"
 
 =end
+
+def build_palindrome(string)
+  current_string = string.dup
+  insertion_index = -1
+  
+  append_index = 0
+
+  loop do
+    if current_string == current_string.reverse
+      return current_string
+    else
+      current_string.insert(insertion_index, string[append_index])
+      insertion_index -= 1
+      append_index += 1
+    end
+  end
+end
+
+
+p build_palindrome("abcdc") == "abcdcba"
+p build_palindrome("abcdcba") == "abcdcba"
+p build_palindrome("ababab") == "abababa"
+p build_palindrome("accccc") == "accccca"
+p build_palindrome("abxdzb") == "abxdzbzdxba"
