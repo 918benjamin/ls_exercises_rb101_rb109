bbe84d686aae5e2fe549fc2584477d67380e0a9b
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-28 06:52:49 -0600

    Add rona5 for study session

---
+++ extra/rona5.rb
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/58942f9175f2c78f4b000108/train/ruby
+=begin
+Given a string str, find the shortest possible string which can be achieved by adding characters to the end of initial string to make it a palindrome.
+
+Example:
+For str = "abcdc", the output should be "abcdcba".
+
+str argument will be between 3 and 10 characters (inclusive)
+
+Test cases:
+build_palindrome("abcdc") == "abcdcba"
+build_palindrome("ababab") == "abababa"
+build_palindrome("accccc") == "accccca"
+
+=end
