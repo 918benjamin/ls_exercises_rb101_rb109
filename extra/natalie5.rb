ed10a8746fb2e8994eab4c79c7ec3046cd9d464e
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-12-28 18:36:40 -0600

    Add natalies solution to the problem

--- extra/natalie5.rb
+++ extra/natalie5.rb
@@ -1,4 +1,5 @@
-https://www.codewars.com/kata/52f78966747862fc9a0009ae/train/ruby
+# https://www.codewars.com/kata/52f78966747862fc9a0009ae/train/ruby
+
 =begin
 Your job is to create a calculator which evaluates expressions in Reverse Polish notation.
 https://en.wikipedia.org/wiki/Reverse_Polish_notation
@@ -25,3 +26,59 @@ calc("5 1 2 + 4 * + 3 -") == 14
 
 
 =end
+## Natalie's solution
+OPERATIONS = ["*", "+", "-", "/"]
+
+# def calc(str)
+#   return 0 if str.empty?
+  
+#   str_ar = str.split
+#   temp_nums = []
+#   final_num = nil
+  
+#   str_ar.each do |ele|
+#     if ele.to_f.to_s == ele
+#       temp_nums << ele.to_f
+#     elsif ele.to_i.to_s == ele
+#       temp_nums << ele.to_f
+#     elsif OPERATIONS.include?(ele)
+#       num1 = temp_nums.pop
+#       final_num.nil? ? num2 = temp_nums.pop : num2 = final_num
+      
+#       final_num  = case ele
+#                    when "*"
+#                      num2 * num1
+#                    when "/"
+#                      num2 / num1
+#                    when "+"
+#                      num2 + num1
+#                    when "-"
+#                      num2 - num1
+#                   end
+    
+#     end
+#   end
+  
+#   final_num.nil? ? temp_nums[0] : final_num
+# end
+
+#code wars solution
+def calc(expr)
+  expr.split.each_with_object([]) { |ele, ar|
+    if ele.match(/\d+/)
+      ar << ele.to_f
+    else
+      y, x = ar.pop, ar.pop
+      ar << x.send(ele, y)
+    end
+  }.last || 0
+end
+
+p calc("") == 0 # Should work with empty string
+p calc("3") == 3 # Should parse numbers
+p calc("3.5") == 3.5 # Should parse float numbers
+p calc("1 3 +") == 4 # Should support addition
+p calc("1 3 *") == 3 # Should support multiplication
+p calc("1 3 -") == -2 # Should support subtraction
+p calc("4 2 /") == 2 # Should support division
+p calc("5 1 2 + 4 * + 3 -") == 14
