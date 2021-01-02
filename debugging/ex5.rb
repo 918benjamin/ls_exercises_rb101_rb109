40fe3e02676dda0ad501241cc94dc829cfd10eed
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-02 13:07:34 -0600

    Add completed ex5 of debugging

--- debugging/ex5.rb
+++ debugging/ex5.rb
@@ -4,6 +4,10 @@
 # Once you get the program to run and produce a sum, you might notice that the
 # sum is off: It's lower than it should be. Why is that?
 
+# On line 30 (original, line 40 below) we don't save the value returned by map.
+# When remaining_cards invokes sum, it isn't summing the card's scores unless we save them to remaining_cards
+# The sum is off because every suit in the deck is referencing the same cards array.
+
 ### Original
 # cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
 
@@ -46,10 +50,10 @@
 ### Debugged
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
 
-deck = { :hearts   => cards,
-         :diamonds => cards,
-         :clubs    => cards,
-         :spades   => cards }
+deck = { :hearts   => cards.dup,
+         :diamonds => cards.dup,
+         :clubs    => cards.dup,
+         :spades   => cards.dup }
 
 def score(card)
   case card
@@ -70,14 +74,16 @@ deck.keys.each do |suit|
   player_cards << cards.pop
 end
 
+p deck
 # Determine the score of the remaining cards in the deck
 
 sum = deck.reduce(0) do |sum, (_, remaining_cards)|
-  remaining_cards.map do |card|
+  remaining_cards.map!  do |card|
     score(card)
   end
 
   sum += remaining_cards.sum
 end
 
+p deck
 puts sum
