765c0ac84ae2bc02b74fd6c44fd793186315c392
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-03 19:55:30 -0600

    Add my solution to watch others code 5

--- extra/watchotherscode5.rb
+++ extra/watchotherscode5.rb
@@ -5,7 +5,52 @@ Example:
 longest_palindrome("I like racecars that go fast") == 7
 
 If the length of the input string is 0, return value must be 0
+
+'baabcd' == 4
+'baabcd' no
+'baabc' no
+'baab' yes
+
+"I like racecars that go fast"
+'I..t' no
+' ..t'
+...
+'r..r'
+
+
+Problem:
+- input: string
+- output: integer
+  - length of the longest substring that is a palindrome
+- single characters count as a palindrome
+- what if there is no palindrome?
+
+1. Get substrings
+  - get string length, len
+  iterate through the characters in string with index
+    - inner = len - index
+    iterate stop times (inner.downto(1) do |stop|
+      - subtring = slice(index, stop)
+      - return substring.length if substring == substring.reverse
+
+
+2. Check if substring is palindrome
+  - string == string.reverse (true if palindrome, false if not)
+
 =end
+
+def longest_palindrome(str)
+  return 0 if str.empty?
+  len = str.length
+  max_len = 0
+  str.chars.each_with_index do |char, index|
+    (len - index).downto(1) do |stop|
+      substring = str.slice(index, stop)
+      max_len = substring.length if substring == substring.reverse && substring.length > max_len
+    end
+  end
+  max_len
+end
 
 puts longest_palindrome('') == 0
 puts longest_palindrome('a') == 1
